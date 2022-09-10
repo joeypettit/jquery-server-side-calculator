d34b4ed3e2f81c6a21f61f987c90c8d7a10a42ef
Joey Pettit <pettitbass@gmail.com>
2022-09-10 01:33:27 -0500

    finished and tested calculation function on server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,31 @@ const app = express();
 // assign PORT
 const PORT = 5000;
 
-//Modules
+// functions
+function getAnswer(object){
+    // ~~~ INCOMING OBJECT ~~~
+    // Object = {
+    //     num1: '',
+    //     num2: '',
+    //     operator: ''
+    // }
+    let num1 = object.num1;
+    let num2 = object.num2;
+    let operator = object.operator;
+    let answer = '';
+
+    if(operator === '+'){
+        answer = Number(num1) + Number(num2);
+    } else if( operator === '-'){
+        answer = Number(num1) - Number(num2);
+    } else if( operator === '*'){
+        answer = Number(num1) * Number(num2);
+    } else if ( operator === '/'){
+        answer = Number(num1) / Number(num2);
+    }
+    // return as a string and round to two decimal places
+    return String(answer.toFixed(2));
+}
 
 
 // set up static file server
@@ -15,27 +39,25 @@ app.use(express.static('server/public'));
 app.use(express.urlencoded({ extended: true }));
 
 
-
-// let calcObject = {
-//     num1: '',
-//     num2: '',
-//     operator: ''
-// }
-function stringToNum(object){
-    let num1 = object.num1;
-    let num2 = object.num2;
-    let operator = object.operator;
-    let answer = '';
-
-    if(operator === '+'){
-        answer = Number(num1) + Number(num2);
-    }
+// ~~~~~~~~ POST REQUESTS ~~~~~~~~~~~
+app.post('/calculate', (req, res) => {
+    let answer = getAnswer(req.body);
+    console.log(req.body);
+    console.log('in /calculate, answer is:', answer);
+    
+})
 
 
 
 
-}
 
+
+
+
+
+
+
+// recieve values from object, calculate, and return answer
 
 
 
@@ -49,9 +71,6 @@ function stringToNum(object){
 
 
 
-
-
-// object coming in
 
 
 
